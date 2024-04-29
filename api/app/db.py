69f7468dd7c545ac60949d12e77208cffd0871db
jhdcruz <jhdcruz@proton.me>
2024-04-29 22:50:13 +0800

    refactor(api): remove explicit `decltypes` in db
    ~ fixes deprecation warnings in test about datetime converters

--- api/app/db.py
+++ api/app/db.py
@@ -55,9 +55,7 @@ def get_db():
     """
 
     if "db" not in g:
-        g.db = sqlite3.connect(
-            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
-        )
+        g.db = sqlite3.connect(current_app.config["DATABASE"])
 
         # return rows that behave like dicts. This allows accessing the columns by name.
         g.db.row_factory = sqlite3.Row
