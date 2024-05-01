5a1b436426b62430ed28ed8c24ea12a8d7ed535f
jhdcruz <jhdcruz@proton.me>
2024-05-02 02:09:07 +0800

    refactor(api): use consistent type naming

--- api/app/sql/schema.sql
+++ api/app/sql/schema.sql
@@ -5,33 +5,33 @@ DROP TABLE IF EXISTS rt_cart_items;
 
 CREATE TABLE rt_users
 (
-    id       INTEGER PRIMARY KEY UNIQUE,
-    name     VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated  TIMESTAMP             DEFAULT NULL
+    id       INT PRIMARY KEY UNIQUE,
+    name     TEXT      NOT NULL,
+    email    TEXT      NOT NULL,
+    username TEXT      NOT NULL,
+    password TEXT      NOT NULL,
+    created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated  TIMESTAMP          DEFAULT NULL
 );
 
 CREATE TABLE rt_products
 (
-    id          INTEGER PRIMARY KEY UNIQUE,
-    name        VARCHAR(255)   NOT NULL,
+    id          INT PRIMARY KEY UNIQUE,
+    name        TEXT           NOT NULL,
     description TEXT           NOT NULL,
-    collection  VARCHAR(255)   NOT NULL,
-    category    VARCHAR(255)   NOT NULL,
-    sx          VARCHAR(255)   NOT NULL,
-    size        VARCHAR(255)   NOT NULL,
+    collection  TEXT           NOT NULL,
+    category    TEXT           NOT NULL,
+    sx          TEXT           NOT NULL,
+    size        TEXT           NOT NULL,
     price       DECIMAL(10, 2) NOT NULL,
-    ratings     INTEGER,
+    ratings     INT,
     created     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE rt_carts
 (
-    id         INTEGER PRIMARY KEY UNIQUE,
+    id         INT PRIMARY KEY UNIQUE,
     user_id    INT       NOT NULL,
     cart_items INT       NOT NULL,
     created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
