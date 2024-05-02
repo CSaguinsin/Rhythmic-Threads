0f0dabd5993a5e433c4cc464bffdad6aff8c11c8
jhdcruz <jhdcruz@proton.me>
2024-05-02 12:09:20 +0800

    test(api): preliminary auth tests

---
+++ api/tests/routes/test_auth.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+@pytest.fixture()
+def default_user(client):
+    """
+    Register a default user for testing.
+
+    :param client:
+    :return:
+    """
+    client.post(
+        "/auth/register",
+        json={
+            "email": "admin@admin.com",
+            "password": "admin",
+            "name": "admin"
+        })
+
+
+def test_register(client):
+    response = client.post(
+        "/auth/register",
+        json={
+            "email": "admin2@admin.com",
+            "password": "admin2",
+            "name": "admin2"
+        })
+    assert response.status_code == 201
+
+
+def test_login(default_user, client):
+    response = client.post(
+        "/auth/login",
+        json={
+            "email": "admin@admin.com",
+            "password": "admin",
+        })
+
+    assert response.status_code == 200
