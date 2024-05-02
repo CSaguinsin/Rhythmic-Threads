e7e0a9339ce0ac5f60ac3fc24979ffb1fd07b4c1
jhdcruz <jhdcruz@proton.me>
2024-05-02 13:40:33 +0800

    fix(db): missing primary keys `id`

--- api/app/sql/schema.sql
+++ api/app/sql/schema.sql
@@ -5,47 +5,47 @@ DROP TABLE IF EXISTS rt_cart_items;
 
 CREATE TABLE rt_users
 (
-    id       INT PRIMARY KEY UNIQUE,
-    name     TEXT      NOT NULL,
-    email    TEXT      NOT NULL,
-    password TEXT      NOT NULL,
-    created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated  TIMESTAMP          DEFAULT NULL
+    id       INTEGER PRIMARY KEY NOT NULL,
+    name     TEXT                NOT NULL,
+    username TEXT                NOT NULL,
+    password TEXT                NOT NULL,
+    created  TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated  TIMESTAMP                    DEFAULT NULL
 );
 
 CREATE TABLE rt_products
 (
-    id          INT PRIMARY KEY UNIQUE,
-    name        TEXT           NOT NULL,
-    description TEXT           NOT NULL,
-    collection  TEXT           NOT NULL,
-    category    TEXT           NOT NULL,
-    sx          TEXT           NOT NULL,
-    size        TEXT           NOT NULL,
-    price       DECIMAL(10, 2) NOT NULL,
-    ratings     INT,
-    created     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id          INTEGER PRIMARY KEY NOT NULL,
+    name        TEXT                NOT NULL,
+    description TEXT                NOT NULL,
+    collection  TEXT                NOT NULL,
+    category    TEXT                NOT NULL,
+    sx          TEXT                NOT NULL,
+    size        TEXT                NOT NULL,
+    price       DECIMAL(10, 2)      NOT NULL,
+    ratings     INTEGER,
+    created     TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated     TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE rt_carts
 (
-    id         INT PRIMARY KEY UNIQUE,
-    user_id    INT       NOT NULL,
-    cart_items INT       NOT NULL,
-    created    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id         INTEGER PRIMARY KEY NOT NULL,
+    user_id    INTEGER             NOT NULL,
+    cart_items INTEGER             NOT NULL,
+    created    TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated    TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES rt_users (uid),
     FOREIGN KEY (cart_items) REFERENCES rt_cart_item (id)
 );
 
 CREATE TABLE rt_cart_items
 (
-    id         INT PRIMARY KEY UNIQUE,
-    cart_id    INT       NOT NULL,
-    product_id INT       NOT NULL,
-    qty        INT       NOT NULL,
-    date_added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id         INTEGER PRIMARY KEY NOT NULL,
+    cart_id    INTEGER             NOT NULL,
+    product_id INTEGER             NOT NULL,
+    qty        INTEGER             NOT NULL,
+    date_added TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (cart_id) REFERENCES rt_carts (id),
     FOREIGN KEY (product_id) REFERENCES rt_products (id)
 );
