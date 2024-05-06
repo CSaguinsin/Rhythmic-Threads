58519d7cd34c51e7eedb2873dc6d5fddfa33c4d6
jhdcruz <jhdcruz@proton.me>
2024-05-07 01:53:41 +0800

    fix(web): fix footer logo color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
-import Logo from "/webSystemLogo.png";
-
 const Footer = () => {
   return (
     <footer className="bg-white dark:bg-gray-900">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <a href="https://flowbite.com/" className="flex items-center">
-              <img src={Logo} className="me-3 h-[8rem]" alt="FlowBite Logo" />
+          <div className="mb-6 flex items-center md:mb-0 md:mt-4">
+            <a
+              href="https://flowbite.com/"
+              className="text-xl hover:text-gray-200"
+            >
+              <span>
+                Rhythmic
+                <br />
+                Threads
+              </span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
@@ -73,9 +78,9 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8 dark:border-gray-700" />
+        <hr className="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
             © 2024{" "}
             <a href="https://flowbite.com/" className="hover:underline">
               Rhythmic Threads
